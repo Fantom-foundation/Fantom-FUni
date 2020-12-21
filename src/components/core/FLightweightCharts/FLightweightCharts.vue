9c4943b7910a09c3b712e22355095fd64c051437
Pavel Spacil <pavel.spacil@gmail.com>
2020-12-21 20:29:51 +0100

    Create `FLightweightCharts` component

---
+++ src/components/core/FLightweightCharts/FLightweightCharts.vue
@@ -0,0 +1,350 @@
+<template>
+    <div class="f-lightweight-chart">
+        <div ref="chartContainer" class="f-lightweight-chart__container"></div>
+    </div>
+</template>
+
+<script>
+import { createChart } from 'lightweight-charts';
+import { isArray, throttle } from '../../../utils';
+
+const WEI = 1000000000000000000;
+
+/**
+ * Lightweight chart series object.
+ * @typedef {Object} LightweightSeries
+ * @property {array} series Data for series.
+ * @property {('line' | 'histogram' | 'area' | 'bar' | 'candlestick')} seriesType Type of series.
+ * @property {('' | 'to-eth')} [transformValues]
+ */
+
+/**
+ * Lightweight charts wrapper.
+ * https://www.tradingview.com/lightweight-charts/
+ */
+export default {
+    name: 'FLightweightCharts',
+
+    props: {
+        /**
+         * Data for chart.
+         * It can be series (array of series data) or object, where keys are unique series ids and values are objects
+         * of type LightweightSeries.
+         * @type {[] | {key: string, series: LightweightSeries}}
+         */
+        series: {
+            type: [Array, Object],
+            default() {
+                return [];
+            },
+        },
+        /**
+         * Type of series.
+         * @type {('line' | 'histogram' | 'area' | 'bar' | 'candlestick')}
+         */
+        seriesType: {
+            type: String,
+            default: 'line',
+            validator: function (_value) {
+                return ['line', 'histogram', 'area', 'bar', 'candlestick'].indexOf(_value) !== -1;
+            },
+        },
+        /** Lightweight chart's options. */
+        options: {
+            type: Object,
+            default() {
+                return {};
+            },
+        },
+        /** Lightweight chart's series options. */
+        seriesOptions: {
+            type: Object,
+            default() {
+                return {};
+            },
+        },
+        /** Chart height. */
+        height: {
+            type: Number,
+            default: 300,
+        },
+        /** Basic line color. */
+        color: {
+            type: String,
+            default: '#286FF6',
+        },
+        /** Default scale top margin */
+        scaleMarginTop: {
+            type: Number,
+            default: 0.1,
+        },
+        /** Default scale bottom margin */
+        scaleMarginBottom: {
+            type: Number,
+            default: 0.03,
+        },
+        /**
+         * @type {('' | 'to-eth')}
+         */
+        transformValues: {
+            type: String,
+            default: '',
+            validator: function (_value) {
+                return ['', 'to-eth'].indexOf(_value) !== -1;
+            },
+        },
+        /**  */
+        fitContent: {
+            type: Boolean,
+            default: false,
+        },
+        /** Disable auto resizing. */
+        noAutoResize: {
+            type: Boolean,
+            default: false,
+        },
+    },
+
+    data() {
+        return {
+            chartOptions: {},
+        };
+    },
+
+    computed: {
+        cSeries() {
+            const { series } = this;
+            let data = {};
+
+            if (isArray(series)) {
+                data.series = {
+                    seriesType: this.seriesType,
+                    seriesOptions: this.seriesOptions,
+                    transformValues: this.transformValues,
+                    series: series,
+                };
+            }
+
+            if (!data.series) {
+                data = { ...series };
+            }
+
+            Object.keys(data).forEach((_seriesKey) => {
+                const item = data[_seriesKey];
+
+                if (item.transformValues) {
+                    if (item.transformValues === 'to-eth') {
+                        item.series = this.transformValuesToEth(item.series);
+                    }
+                }
+            });
+
+            return data;
+        },
+    },
+
+    created() {
+        /** Lightweight chart instance. */
+        this._chart = null;
+        /** Array of Lightweight chart series instances. */
+        this._series = {};
+        /** Debounce function used as window resize callback. */
+        this._resizeCallback = throttle((_event) => this.onWindowResize(_event), 300, true);
+    },
+
+    mounted() {
+        this.initChart();
+
+        if (!this.noAutoResize) {
+            window.addEventListener('resize', this._resizeCallback, false);
+        }
+    },
+
+    beforeDestroy() {
+        this.destroyChart();
+
+        if (!this.noAutoResize) {
+            window.removeEventListener('resize', this._resizeCallback);
+        }
+    },
+
+    methods: {
+        transformValuesToEth(_series) {
+            let data = [];
+            let item;
+
+            if (_series) {
+                for (let i = 0, len1 = _series.length; i < len1; i++) {
+                    item = _series[i];
+
+                    data.push({
+                        ...item,
+                        value: parseInt(item.value, 16) / WEI,
+                    });
+                }
+            }
+
+            return data;
+        },
+
+        initChart() {
+            const defaultOptions = {
+                height: this.height,
+                priceScale: {
+                    scaleMargins: {
+                        top: this.scaleMarginTop,
+                        bottom: this.scaleMarginBottom,
+                    },
+                },
+            };
+
+            this.destroyChart();
+
+            this.chartOptions = {
+                ...defaultOptions,
+                ...this.options,
+            };
+
+            this._chart = createChart(this.$refs.chartContainer, this.chartOptions);
+
+            Object.keys(this.cSeries).forEach((_seriesKey) => {
+                this.addSeries(_seriesKey);
+            });
+
+            this.updateColors();
+
+            if (this.fitContent) {
+                this._chart.timeScale().fitContent();
+            }
+        },
+
+        addSeries(_seriesKey) {
+            const chart = this._chart;
+            const seriesObj = this.cSeries[_seriesKey];
+
+            if (!chart || !seriesObj) {
+                return;
+            }
+
+            const defaultSeriesOptions = {
+                // color: this.color,
+            };
+            const seriesOptions = {
+                ...defaultSeriesOptions,
+                ...seriesObj.seriesOptions,
+            };
+            let series = null;
+
+            switch (seriesObj.seriesType) {
+                case 'line':
+                    series = chart.addLineSeries(seriesOptions);
+                    break;
+                case 'histogram':
+                    series = chart.addHistogramSeries(seriesOptions);
+                    break;
+                case 'area':
+                    series = chart.addAreaSeries(seriesOptions);
+                    break;
+                case 'bar':
+                    series = chart.addBarSeries(seriesOptions);
+                    break;
+                case 'candlestick':
+                    series = chart.addCandlestickSeries(seriesOptions);
+                    break;
+            }
+
+            if (series) {
+                series.setData(seriesObj.series);
+                this._series[_seriesKey] = series;
+            }
+        },
+
+        updateColors() {
+            const chartCP = this.getChartCustomProperties();
+            const series = this._series;
+            const { cSeries } = this;
+            const { chartOptions } = this;
+            const options = {
+                grid: {},
+                layout: {},
+            };
+            const grid = { ...chartOptions.grid };
+            const layout = { ...chartOptions.layout };
+
+            if (chartCP.vertLinesColor && (!grid.vertLines || !grid.vertLines.color)) {
+                options.grid.vertLines = { color: chartCP.vertLinesColor };
+            }
+
+            if (chartCP.horzLinesColor && (!grid.horzLines || !grid.horzLines.color)) {
+                options.grid.horzLines = { color: chartCP.horzLinesColor };
+            }
+
+            ['backgroundColor', 'textColor', 'fontSize', 'fontFamily'].forEach((_prop) => {
+                if (chartCP[_prop] && !layout[_prop]) {
+                    options.layout[_prop] = _prop === 'fontSize' ? parseInt(chartCP[_prop]) : chartCP[_prop];
+                }
+            });
+
+            this._chart.applyOptions(options);
+
+            Object.keys(series).forEach((_seriesKey) => {
+                const item = cSeries[_seriesKey];
+                const seriesOptions = { ...item.seriesOptions };
+
+                if (!item) {
+                    return;
+                }
+
+                if (chartCP.lineColor && (!seriesOptions || !seriesOptions.color)) {
+                    series[_seriesKey].applyOptions({ color: chartCP.lineColor });
+                }
+            });
+        },
+
+        getChartCustomProperties() {
+            const style = getComputedStyle(this.$el);
+            const cp = {
+                lineColor: style.getPropertyValue('--f-lightweight-chart-chart-line-color'),
+                textColor: style.getPropertyValue('--f-lightweight-chart-chart-text-color'),
+                vertLinesColor: style.getPropertyValue('--f-lightweight-chart-chart-vert-lines-color'),
+                horzLinesColor: style.getPropertyValue('--f-lightweight-chart-chart-horz-lines-color'),
+                backgroundColor: style.getPropertyValue('--f-lightweight-chart-chart-background-color'),
+                fontSize: style.getPropertyValue('--f-lightweight-chart-chart-font-size'),
+                fontFamily: style.getPropertyValue('--f-lightweight-chart-chart-font-family'),
+            };
+
+            Object.keys(cp).forEach((_key) => {
+                const color = cp[_key];
+
+                if (color) {
+                    cp[_key] = color.trim();
+                } else {
+                    delete cp[_key];
+                }
+            });
+
+            return cp;
+        },
+
+        destroyChart() {
+            if (this._chart) {
+                this._series = {};
+                this._chart.remove();
+                this._chart = null;
+            }
+        },
+
+        onWindowResize() {
+            const chart = this._chart;
+
+            if (chart) {
+                chart.resize(this.$refs.chartContainer.clientWidth, this.height);
+            }
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+@import 'style';
+</style>
