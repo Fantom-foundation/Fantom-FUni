8e8eec5027027b700db6df7c634f6286e920e90f
Pavel Spacil <pavel.spacil@gmail.com>
2022-04-25 08:02:32 +0200

    Update list of API servers

--- app.config.js
+++ app.config.js
@@ -19,7 +19,7 @@ const appConfig = {
                 // for subscriptions
                 ws: '',
             },
-            {
+            /*{
                 http: 'https://xapi-nodec.fantom.network/',
                 // for subscriptions
                 ws: '',
@@ -38,7 +38,7 @@ const appConfig = {
                 http: 'https://xapi-nodef.fantom.network/',
                 // for subscriptions
                 ws: '',
-            },
+            },*/
         ],
         // index into providers array of default provider or 'random' - takes index randomly
         defaultProviderIndex: 'random',
